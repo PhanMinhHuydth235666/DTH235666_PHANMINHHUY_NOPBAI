a5bb298fb9aaee0848e1a92208d875b693482120
DTH235666PHANMINHHUY <huym60566@gmail.com>
2025-08-28 13:56:06 +0700

    Add files via upload

---
+++ baitap.py
@@ -0,0 +1,21 @@
+print('một người không bao giờ mắc sai lầm không bao giờ thử điều gì mới - baitap.py:1')
+
+if 5 > 2:
+    print('five is greater than two! - baitap.py:4')
+
+    total =1 + \
+    2+ \
+    3
+print(total); print('hello python - baitap.py:9')
+
+if True:
+    print('true - baitap.py:12')
+else:
+    print('false - baitap.py:14')  
+
+    #this is a comment
+    print("Hello, World! - baitap.py:17") 
+
+    def add(a, b):
+     result = a + b
+     return result

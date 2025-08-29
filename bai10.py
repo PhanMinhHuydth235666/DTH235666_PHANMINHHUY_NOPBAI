e7437485d063437578d6fb545c5e1df7d962dc9e
DTH235666PHANMINHHUY <huym60566@gmail.com>
2025-08-29 13:10:41 +0700

    Add files via upload

---
+++ bai10.py
@@ -0,0 +1,16 @@
+# Phần ngọn cây
+print("* - bai10.py:2")
+print("* - bai10.py:3")
+print("***** - bai10.py:4")
+
+# Phần thân cây (3 tầng)
+for i in range(2, 5):
+    stars = "*" * (i * 2 + 1)
+    spaces = " " * (6 - len(stars) // 2)
+    print(spaces + stars)
+
+# Phần đáy cây
+print("** - bai10.py:13")
+print("** - bai10.py:14")
+Bạn đã gửi
+9

ff8594d91ee0c7d72f276555e094feed4f2354af
DTH235666PHANMINHHUY <huym60566@gmail.com>
2025-08-29 13:46:33 +0700

    Update bai3c2.py

--- bai3c2.py
+++ bai3c2.py
@@ -3,5 +3,7 @@ ly = float(input("Nhập điểm Lý: "))
 hoa = float(input("Nhập điểm Hóa: "))
 
 dtb = (toan + ly + hoa) / 3
-print("Điểm trung bình = - bai3c2.py:6", dtb)
-print("Điểm trung bình (làm tròn 2 số lẻ) = - bai3c2.py:7", round(dtb, 2))
+print("Điểm trung bình =", dtb)
+print("Điểm trung bình (làm tròn 2 số lẻ) =", round(dtb, 2))
+
+

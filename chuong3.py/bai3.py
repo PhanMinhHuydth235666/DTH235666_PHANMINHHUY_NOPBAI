81106ce403311326b20e9f34682f67d1784fccbf
PhanMinhHuy_DTH235666 <huy_dth235666@student.agu.edu.vn>
2025-09-18 21:15:00 +0700

    Update bai3.py

--- chuong3.py/bai3.py
+++ chuong3.py/bai3.py
@@ -1,23 +1,23 @@
 from math import sqrt
-print("Chương trình Giải Phương trình bậc 2 - bai3.py:2")
+print("Chương trình Giải Phương trình bậc 2")
 a = float(input("Nhập a: "))
 b = float(input("Nhập b: "))
 c = float(input("Nhập c: "))
 if a == 0:
     if b == 0 and c == 0:
-        print("Vô số nghiệm - bai3.py:8")
+        print("Vô số nghiệm ")
     elif b == 0 and c != 0:
-        print("Vô nghiệm - bai3.py:10")
+        print("Vô nghiệm")
     else:
         x = -c / b
-        print("Nghiệm x = - bai3.py:13", x)
+        print("Nghiệm x =", x)
 else:
     delta = b**2 - 4*a*c
     if delta < 0:
-        print("Vô nghiệm - bai3.py:17")
+        print("Vô nghiệm")
     elif delta == 0:
         x = -b / (2*a)
-        print("Nghiệm kép x1 = x2 = - bai3.py:20", x)
+        print("Nghiệm kép x1 = x2 =", x)
     else:
         x1 = (-b - sqrt(delta)) / (2*a)
         x2 = (-b + sqrt(delta)) / (2*a)

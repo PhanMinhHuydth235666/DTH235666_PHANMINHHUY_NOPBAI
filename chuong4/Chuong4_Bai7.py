871b6fb286f432edbcacd850d575a8d5d5248f2b
PhanMinhHuy_DTH235666 <huy_dth235666@student.agu.edu.vn>
2025-09-25 22:10:25 +0700

    Update Chuong4_Bai7.py

--- chuong4/Chuong4_Bai7.py
+++ chuong4/Chuong4_Bai7.py
@@ -2,13 +2,13 @@ import math
 def DoDai(xA, yA, xB, yB):
     return math.sqrt((xB - xA)**2 + (yB - yA)**2)
 
-print("Chương trình tính độ dài đoạn AB - Chuong4_Bai7.py:5")
-print("Nhập điểm A(x,y): - Chuong4_Bai7.py:6")
+print("Chương trình tính độ dài đoạn AB ")
+print("Nhập điểm A(x,y): ")
 xA = float(input("Nhập xA: "))
 yA = float(input("Nhập yA: "))
 
-print("Nhập điểm B(x,y): - Chuong4_Bai7.py:10")
+print("Nhập điểm B(x,y): ")
 xB = float(input("Nhập xB: "))
 yB = float(input("Nhập yB: "))
 
-print("Độ dài cạnh |AB| = - Chuong4_Bai7.py:14", DoDai(xA,  yA, xB, yB))
+print("Độ dài cạnh |AB| = ", DoDai(xA,  yA, xB, yB))

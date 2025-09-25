37538d8d457d2d72bc28d68790deb806f9a0aaab
PhanMinhHuy_DTH235666 <huy_dth235666@student.agu.edu.vn>
2025-09-25 22:08:30 +0700

    Update Chuong4_Bai4.py

--- chuong4/Chuong4_Bai4.py
+++ chuong4/Chuong4_Bai4.py
@@ -3,13 +3,13 @@ def ROI(dt,cp):
 
 def GoiYRoi(roi):
     if roi >= 0.75:
-        print("==> Nên đầu tư - Chuong4_Bai4.py:6")
+        print("==> Nên đầu tư ")
     else:
-        print("==> Không nên đầu tư - Chuong4_Bai4.py:8")
+        print("==> Không nên đầu tư ")
 
-print("Chương trình tính ROI - Chuong4_Bai4.py:10")
+print("Chương trình tính ROI ")
 dt = int(input("Nhập doanh thu: "))
 cp = int(input("Nhập chi phí: "))
 roi = ROI(dt,cp)
-print("Tỉ lệ ROI: - Chuong4_Bai4.py:14", roi)
+print("Tỉ lệ ROI: ", roi)
 GoiYRoi(roi)
